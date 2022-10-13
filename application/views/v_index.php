a85b2bd08f69d97f4615bcc8acb90ed0567d533c
12210182- Josua Pardamean Ritonga <josuaritonga5@gmail.com>
2022-10-13 20:04:10 +0700

    Menambahkan Database

---
+++ application/views/v_index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Berita Loker | Loker Update</title>	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/style.css">
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<hgroup>
+				<h1>Berita Loker</h1>
+				<h3>Loker Update</h3>
+			</hgroup>
+			<nav>
+				<ul>
+				<li><a href="<?php echo base_url().'index.php/web/login' ?>">Login Validasi Data</a></li>
+					<li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+					<li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
+					<li><a href="<?php echo base_url().'index.php/web/bio'?>">Data</a></li>
+				</ul>
+			</nav>
+			<div class="clear"></div>
+		</header>
+		<section>
+		<h1><?php echo $judul ?></h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		</section>
+		<footer>						
+		</footer>
+	</div>
+</body>
+</html>
